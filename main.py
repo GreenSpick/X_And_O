3997eb94fda16b53e4bac99216e67fe8ce323c1d
JojoPohuist <payebolnikov@mail.ru>
2022-04-09 00:39:45 +0900

    Fix bug

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ def Draw():
         print("-------------")
 
 def Restart():
-    global field, compare_field, restart
+    global field, compare_field, round
     compare_field = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    round = 0
+    print("game restarting")
     Draw()
 
 def Compare(a):
@@ -18,11 +20,14 @@ def Compare(a):
             print("\nWin " + a)
             Restart()
 
+
 def Turn(b, c):
     while True:
+        global round
         a = input(c)
         if (field[int(int(a) // 3.1)][(int(a) % 3)-1]) != ("X" or "O"):
             field[int(int(a) // 3.1)][(int(a) % 3)-1] = b
+            round += 1
         else:
             print("\n" * 3, "You can't go there")
             Draw()
@@ -32,9 +37,12 @@ def Turn(b, c):
 
 compare_field = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+round = 0
 Draw()
 
 while True:
+    if round >=8:
+        Restart()
     Turn("X", "\nPut X : ")
     Compare("X")
     Turn("O", "\nPut O : ")
